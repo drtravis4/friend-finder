579cd56209dc0cbd097f3d18aaca9812b6eec7aa
Travis Bunn <drtravis4@gmail.com>
2020-04-12 11:27:38 -0500

    finished friend finder app

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express")
+var port = process.env.PORT || 8000
+var app = express()
+app.use (express.static("app/public"))
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+var apiRoutes = require("./app/routing/apiRoutes")
+apiRoutes(app)
+var htmlRoutes = require("./app/routing/htmlRoutes")
+htmlRoutes(app)
+app.listen(port, function(){
+    console.log("app is listening on http://localhost:" + port)
+})
